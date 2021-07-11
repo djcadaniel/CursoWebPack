7b147d6dca1df5d69c35aaa5af39c370b8acb34c
djcadaniel <djcadaniel@gmail.com>
2021-07-11 10:05:57 -0500

    terminado ok hasta loaders fuentes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')  //ya esta disponible en node, no es necesario instalar dependencias
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 //el modulo tendtra un objeto
 module.exports = {
     entry: './src/index.js',//punto de entrada de nuestra app
     output: { //hacia donde vamos a enviar
         path: path.resolve(__dirname, 'dist'), //cuando subimos nuestro  proyecto a la nube, no tendremos problemas al ubicar el archivo principal
-        filename: 'main.js'
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]' //moviendo nuestras imagenes a dist
     },
     //extensiones con lo que vamos a trabajar
     resolve: {
@@ -29,6 +31,24 @@ module.exports = {
                 'css-loader',
                 'stylus-loader'
                 ],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use:{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "aplication/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule: false,
+                    }
+                }
             }
         ]
     },
@@ -39,5 +59,13 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),//hacia donde vamos a copiar
+                    to: "assets/images"
+                }
+            ]
+        })
     ]
 }
